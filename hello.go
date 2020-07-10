f80ea1c9c8c6271d6f73abc56a30586705f0ee9a
Dan Katz <dgkatz@gmail.com>
2020-07-10 16:08:56 -0400

    greeting message with for loop

--- hello.go
+++ hello.go
@@ -2,10 +2,29 @@ package main
 
 import "fmt"
 
-func printName(name string) {
-	fmt.Printf("Hello, World! %s\n", name)
+const greetingTemplate string = "Hello, %v for the %v%v time"
+
+func countSuffix(count int) (countSuffix string){
+	lastDigit := count % 10
+	if lastDigit == 1 && count != 11 {
+		countSuffix = "st"
+	} else if lastDigit == 2 && count != 12{
+		countSuffix = "nd"
+	} else if lastDigit == 3 && count != 13{
+		countSuffix = "rd"
+	} else {
+		countSuffix = "th"
+	}
+	return
+}
+
+func printName(name string, count int) {
+	for i := 1; i <= count; i++ {
+		greeting := fmt.Sprintf(greetingTemplate, name, i, countSuffix(i))
+		fmt.Println(greeting)
+	}
 }
 
 func main() {
-	printName("dan")
+	printName("Dan", 30)
 }
